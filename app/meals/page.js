a6244fc02400e8eea368fbaf1c81071efabee8bc
bvp1518 <bvp18061992@gmail.com>
2024-09-20 12:01:44 +0530

    till-433. Adding A Loading Page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,4 +1,30 @@
-export default function MealsPages(){
-    console.log('Meals page')
-    return <h1>Meals Page</h1>
+
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+export default async function MealsPages() {
+  const meals = await getMeals();
+
+    return (
+        <>
+            <header className={classes.header}>
+                <h1 className={classes.title}>
+                    Delicious meals,created{' '}
+                    <span className={classes.highlight}>by you</span>
+                    <main className={classes.main}></main>
+                </h1>
+                <p>Choose your favourite recipe and cook it yourself.It is easy and fun!</p>
+                <p className={classes.cta}>
+                <Link href="/meals/share">
+                  share you favourite recipe
+                </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={meals}/>
+            </main>
+        </>
+    )
 }
